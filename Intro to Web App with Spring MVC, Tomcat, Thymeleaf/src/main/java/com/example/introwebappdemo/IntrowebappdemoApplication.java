d47478f583f3bf0d44a53e03acca161e957d3b24
danny <morganz55057@gmail.com>
2020-01-22 19:54:09 +0100

    added second project intro web app

---
+++ Intro to Web App with Spring MVC, Tomcat, Thymeleaf/src/main/java/com/example/introwebappdemo/IntrowebappdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.introwebappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrowebappdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntrowebappdemoApplication.class, args);
+    }
+
+}
---
+++ Intro to Web App with Spring MVC, Tomcat, Thymeleaf/src/test/java/com/example/introwebappdemo/IntrowebappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.introwebappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrowebappdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
